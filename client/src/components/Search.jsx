404b1f8115f0527136fde2df3e046758a15e9366
pauldev1128 <pauldev118@gmail.com>
2019-08-16 16:37:37 -0700

    worked on connecting and adding to the databse using postman

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Search extends React.Component {
   constructor(props) {
@@ -6,6 +7,8 @@ class Search extends React.Component {
     this.state = {
       term: ''
     }
+    this.onChange = this.onChange.bind(this);
+    this.search = this.search.bind(this);
   }
 
   onChange (e) {
@@ -14,6 +17,7 @@ class Search extends React.Component {
     });
   }
 
+
   search() {
     this.props.onSearch(this.state.term);
   }
@@ -21,9 +25,9 @@ class Search extends React.Component {
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>
       <button onClick={this.search}> Add Repos </button>
-    </div>) 
+    </div>)
   }
 }
 
--- database/index.js
+++ database/index.js
@@ -1,16 +1,30 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("database connected")
+});
+
+
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  name: String,
+  owner: {
+    login: String,
+    url: String
+  },
+  watchers: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (data) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  return Repo.create(data)
 }
 
 module.exports.save = save;
